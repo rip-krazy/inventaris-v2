4bff0d15db0dbb29b8af83ee10f3062377bb54d0
anisaaaaa119 <anisasulistiawati87@gmail.com>
2024-11-21 12:53:08 +0700

    gsfghahsgc

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="id">
    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="csrf-token" content="{{ csrf_token() }}">
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+      <title>Welcome to Inventaris Barang</title>
   </head>
 <body class="bg-gray-100 flex flex-col h-screen">
 <header class="bg-gray-800 shadow p-4 flex justify-between items-center relative">
