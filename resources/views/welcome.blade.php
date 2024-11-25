8429086efad092166c553c40d88466ce15ba2518
rip-krazy <herlambang731@gmail.com>
2024-11-25 10:39:09 +0700

    npm welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
 <header class="bg-gray-800 shadow p-4 flex justify-between items-center">
