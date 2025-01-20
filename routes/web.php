0a0191a622c5a3ac644f4a6dfce49f849c9a0d21
yaa1203 <muhammadilyahamzah@gmail.com>
2025-01-20 14:25:04 +0700

    jhgjdkghjd

--- routes/web.php
+++ routes/web.php
@@ -88,7 +88,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
