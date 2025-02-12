42693ac093e01760514e864ec40696351fb80972
yaa1203 <muhammadilyahamzah@gmail.com>
2025-02-12 21:15:38 +0700

    pp

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -60,9 +60,9 @@
         </div>
         <div class="header-dropdown hidden sm:block relative">
             <button id="avatarBtn" class="focus:outline-none">
-                <div class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-200 bg-transparent hover:bg-gray-700 focus:outline-none transition">
+                <div class="inline-flex items-center px-3 py-2 border border-transparent text-lg leading-4 font-medium rounded-md text-gray-200 bg-transparent hover:bg-gray-700 focus:outline-none transition">
                     {{ Auth::user()->name }}
-                    <svg class="ml-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <svg class="ml-1 mt-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                     </svg>
                 </div>
