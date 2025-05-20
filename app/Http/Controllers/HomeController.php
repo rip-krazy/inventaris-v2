745a5da287ec3d2e4a1a31432ea1de0852a88fe6
FATTFAWW <164737174+FATTFAWW@users.noreply.github.com>
2025-05-20 14:11:46 +0700

    bassing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\Pengguna;
 use App\Models\Barang;
 use App\Models\History;
 use Illuminate\Support\Facades\Auth;
